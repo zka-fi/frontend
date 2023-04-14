d9f2c21c351296a8dbad6a7c79a1ef90d0d71317
Sam <sam03060027@gmail.com>
2023-04-14 22:33:04 +0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const nextConfig = {
+  reactStrictMode: true,
+
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+  
+  webpack: function (config, options) {
+    if (!options.isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    config.experiments = { asyncWebAssembly: true, layers: true };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { ConnectKitButton } from 'connectkit'
+import { useAccount } from 'wagmi'
+
+import { Account } from '../components'
+
+function Page() {
+  const { isConnected } = useAccount()
+  return (
+    <>
+      <ConnectKitButton />
+      {isConnected && <Account />}
+    </>
+  )
+}
+
+export default Page
